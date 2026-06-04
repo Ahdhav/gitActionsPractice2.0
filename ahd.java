5ba891888a3454f8ad823803e105fee0b262b2bc
Ahdhav Suresh <karaikayal@gmail.com>
2026-06-04 10:11:34 -0400

    Add package declaration and private constructor

--- ahd.java
+++ ahd.java
@@ -1,7 +1,15 @@
 /**
  * Main application class.
  */
+package ahd;
+
 public class Ahd {
+  /**
+   * Private constructor to hide the implicit public one.
+   */
+  private Ahd() {
+  }
+
   /**
    * Main method entry point.
    *
