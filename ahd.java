16bb45663fe014ee881def42fd52bf0fd03c018d
Ahdhav Suresh <karaikayal@gmail.com>
2026-06-04 10:04:03 -0400

    Refactor ahd class and add Javadoc comments
    Refactor class name and add documentation comments.

--- ahd.java
+++ ahd.java
@@ -1,7 +1,13 @@
-public class ahd{
-  
-  public static void main (String args[]){
+/**
+ * Main application class.
+ */
+public class Ahd {
+  /**
+   * Main method entry point.
+   *
+   * @param args the command line arguments
+   */
+  public static void main(final String[] args) {
     System.out.println("Hello World");
   }
-
 }
